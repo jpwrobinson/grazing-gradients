a6c7383939370f72925b8cec3a5615d7db5024ec
Jan-Claas Dajka <j.dajka@lancaster.ac.uk>
2018-05-16 15:28:07 +0100

    updates Maldives Rscript

--- data/maldives_script.R
+++ data/maldives_script.R
@@ -8,6 +8,11 @@ maldives <- herb[herb$dataset == 'Maldives',]
 ## write to csv
 write.csv(maldives, file = 'data/maldives_master.csv')
 
+
+setwd("~/Box Sync/PhD Feedbacks/Git Repository/grazing-gradients/data")
+load('maldives_master.csv')
+maldives <- maldives_master
+
 str(maldives)
 unique(maldives$FG)
 unique(maldives$family)
@@ -15,10 +20,19 @@ unique(maldives$species)
 unique(maldives$habitat)
 unique(maldives$management)
 
-hist(maldives$biomass.kgha[maldives$FG=="Herbivore Grazer"], breaks = 30, main = "Grazers")
-hist(maldives$biomass.kgha[maldives$FG=="Herbivore Scraper"], breaks = 60, main = "Scrapers")
-hist(maldives$biomass.kgha[maldives$FG=="Herbivore Browser"], breaks = 30, main = "Browsers")
+hist(maldives$biomass.kgha[maldives$FG=="Herbivore Grazer"], 
+     breaks = 30, main = "Grazers")
+hist(maldives$biomass.kgha[maldives$FG=="Herbivore Scraper"], 
+     breaks = 60, main = "Scrapers")
+hist(maldives$biomass.kgha[maldives$FG=="Herbivore Browser"], 
+     breaks = 30, main = "Browsers")
 
+dotchart(maldives$biomass.kgha[maldives$FG=="Herbivore Grazer"], main = "Grazers")
+dotchart(maldives$biomass.kgha[maldives$FG=="Herbivore Scraper"],main = "Scrapers")
+dotchart(maldives$biomass.kgha[maldives$FG=="Herbivore Browser"], main = "Browsers")
+
+
+#fancy James coding
 hist(log2(maldives$biomass.kgha[maldives$FG=="Herbivore Grazer"]), main = "Grazers", axes =F)
 axis(1, at=c(-1:5), labels=2^(c(-1:5))); axis(2)
 hist(log2(maldives$biomass.kgha[maldives$FG=="Herbivore Scraper"]), main = "Scrapers", axes = F)
