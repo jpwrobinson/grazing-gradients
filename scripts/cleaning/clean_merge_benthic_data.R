82830033aa0430039f787485071d19d53aaa5b1f
James Robinson <jpwrobinson@gmail.com>
2018-05-31 14:07:39 +0100

    maldives done, started GBR. see issue #8 for running issues

--- scripts/cleaning/clean_merge_benthic_data.R
+++ scripts/cleaning/clean_merge_benthic_data.R
@@ -9,7 +9,7 @@ load('data/wio_gbr_herb_master.Rdata')
 ## load uncleaned benthic data
 maldives<-read.csv('data/raw-data/csv/maldives_benthos.csv')
 gbr<-read.csv('data/raw-data/csv/gbr_benthos.csv')
-chagos<-read.csv('data/raw-data/csv/chagos_benthos.csv')
+chagos<-read.csv('data/raw-data/csv/chagos_benthos.csv', header=F)
 
 ## load seychelles (clean)
 load('data/raw-data/SEY_UVC_benthicPV_SC_DEPTH_replicates.Rdata')
@@ -22,6 +22,9 @@ seychelles$dataset<-'Seychelles'
 
 ## gather taxa into 1 column
 seychelles<-gather(seychelles,  taxa, value, -site, -habitat, -transect, -year, -depth, -site.year, -reef, -state)
+
+## drop extra taxa for now to focus on total hard.coral and macroalgae
+
 
 ## ------------------------------------ ##
 		   ## clean maldives ##
@@ -40,7 +43,7 @@ other<-c('Fungia', 'Soft Coral Encrusting', 'Zoanthid', 'Hydroid', 'Ascidian',
 	'Heliopora', 'Soft Coral Table')
 algae<-c('CCA', 'EAM', 'Macroalgae')
 
-maldives$benthic<-ifelse(maldives$taxa %in% other, 'other', 'hardcoral')
+maldives$benthic<-ifelse(maldives$taxa %in% other, 'other', 'hard.coral')
 maldives$benthic<-ifelse(maldives$taxa %in% availsubtrate, 'availablesubtrate', maldives$benthic)
 maldives$benthic<-ifelse(maldives$taxa %in% algae, 'algae', maldives$benthic)
 
@@ -59,6 +62,35 @@ maldives %>% group_by(dataset, site, date, reef, transect, depth) %>%
 		   ## clean chagos ##
 ## ------------------------------------ ##
 
+head(chagos)[1]
+
+
 ## ------------------------------------ ##
 		   ## clean GBR ##
 ## ------------------------------------ ##
+colnames(gbr)[1:6]<-c('date', 'site', 'site.number', 'habitat', 'depth', 'transect')
+
+## gather taxa into 1 column
+gbr <- gather(gbr, taxa, value, -date, -site, -site.number, -habitat, -depth, -transect)
+gbr$dataset<-'GBR'
+gbr$reef<-"NA"
+## estimates are already percent cover
+## need to group taxa for simpler comparison across datasets + benthic gradients
+availsubtrate<-c('Pavement', 'Sand', 'Rubble', 'Bare.Substrate')
+# other<-c('Fungia', 'Soft Coral Encrusting', 'Zoanthid', 'Hydroid', 'Ascidian', 
+# 	'Sponge Encrusting', 'Anemone', 'Coralimorph', 'Tubastrea', 
+# 	'Heliopora', 'Soft Coral Table')
+algae<-c('Turf', 'Turfs','Macroalgae', 'CCA.1', 'Unidentified.Macroalgae', 'Halimeda', 'Filimentous.Green',
+			'Ulva', 'CCA', 'EAM') 
+
+gbr$benthic[gbr$taxa %in% availsubtrate]<- 'availablesubtrate'
+gbr$benthic[gbr$taxa %in% algae]<- 'algae'
+gbr$benthic[gbr$taxa == 'Total.hard.coral.cover']<- 'hard.coral'
+
+## drop missing categories (i.e. non coral, algal or substrate groups)
+gbr<-gbr[!is.na(gbr$benthic),]
+
+## sum groups within each benthic category
+gbr <- gbr %>% group_by(dataset, site, date, reef, transect, depth, benthic) %>%
+			summarise(value = sum(value))
+
