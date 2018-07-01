adfde450746f5a3b92d3dd8e4649af955822a953
Jeneen Hadj-Hammou <jeneenhh@gmail.com>
2018-07-01 13:56:57 +0100

    basic graph FG across bethic gradients

---
+++ scripts/explore/FG_benthos_gradients.R
@@ -0,0 +1,34 @@
+setwd("~/Documents/git_repos/grazing-gradients")
+load("/data/wio_herb_benthic_merged.Rdata")
+
+library(ggplot2)
+
+#looking at biomass of FG along coral cover gradient
+# Change point shapes and colors = U-G-L-Y
+pred$logbiom <- log(pred$biomass.kgha)
+ggplot(pred, aes(x=hard.coral, y=logbiom, shape=FG, color=FG)) +
+  geom_point()
+
+#split pannels with points
+ggplot(pred, aes(hard.coral, biomass.kgha)) +
+  geom_point() +
+  geom_smooth(span = 0.8) +
+  facet_wrap(~FG)
+
+#split pannels without points
+ggplot(pred, aes(hard.coral, biomass.kgha)) +
+  geom_smooth(span = 0.8) +
+  facet_wrap(~FG)
+
+#FG across macroalgae gradient
+ggplot(pred, aes(macroalgae, logbiom)) +
+  #geom_point() +
+  geom_smooth(span = 0.8) +
+  facet_wrap(~FG)
+
+#FG across complexity gradient
+ggplot(pred, aes(complexity, logbiom)) +
+  #geom_point() +
+  geom_smooth(span = 0.8) +
+  facet_wrap(~FG)
+
