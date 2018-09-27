afc4f7c8f0a8bc4a66d83d04725e890b7d429239
JamieMcDevittIrwin <jamie.mcirwin@gmail.com>
2018-09-27 12:38:42 -0400

    biplot and screeplot of overall PCA

--- scripts/explore/benthic-regimes-explore.R
+++ scripts/explore/benthic-regimes-explore.R
@@ -9,9 +9,14 @@ rm(list=ls())
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(plyr)
 library(vegan)
 library(grid)
 library(gridExtra)
+library(factoextra)
+library(ggbiplot)
+library(ggfortify)
+
 
 # load data
 load("data/wio_herb_benthic_merged.Rdata")
@@ -19,7 +24,6 @@ ls()
 head(pred)
 dim(pred)
 str(pred)
-pred$unique.id
 ############################################################
 
 
@@ -30,18 +34,18 @@ pred$unique.id
 # Site= unique.id? or also by transect? 
 # Need to make dataframe: site by habitat type 
 
-pred <-pred %>% select(unique.id, hard.coral, macroalgae, rubble, 
+pred <-pred %>% select(unique.id,dataset,hard.coral, macroalgae, rubble, 
                        substrate, complexity, dataset)
 head(pred) 
 
 # Average across transects within a site
-pred <- pred %>%
-    group_by(unique.id) %>%
-    summarise(hard.coral.mean = mean(hard.coral),
-              macroalgae.mean = mean(macroalgae),
-              rubble.mean = mean(rubble),
-              substrate.mean = mean(substrate),
-              complexity.mean = mean(complexity))
+pred <- pred %>% 
+  dplyr::group_by(unique.id,dataset) %>% 
+  dplyr::summarize(hard.coral.mean = mean(hard.coral),
+            macroalgae.mean = mean(macroalgae),
+            rubble.mean = mean(rubble),
+            substrate.mean = mean(substrate),
+            complexity.mean = mean(complexity))
 head(pred)
 str(pred)
 
@@ -59,23 +63,36 @@ head(pred)
 
 
 ############################################################
-### Plot ###
+### Overall PCA (all regions) ###
 
-df <- pred[-1]
-head(df)
+# set blank theme
+theme<-theme(panel.background = element_blank(),
+             panel.border=element_rect(fill=NA),
+             panel.grid.major = element_blank(),
+             panel.grid.minor = element_blank(),
+             strip.background=element_blank(),
+             axis.text.x=element_text(colour="black"),
+             axis.text.y=element_text(colour="black"),
+             axis.ticks=element_line(colour="black"),
+             plot.margin=unit(c(1,1,1,1),"line"))
 
-theme<-theme(panel.background = element_blank(),panel.border=element_rect(fill=NA),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),strip.background=element_blank(),axis.text.x=element_text(colour="black"),axis.text.y=element_text(colour="black"),axis.ticks=element_line(colour="black"),plot.margin=unit(c(1,1,1,1),"line"))
+# PCA: 2 dimensions
+pred_pca <- prcomp(pred[-1], scale.=TRUE) # take out the dataset column
+pred_pca
 
-df_pca <- prcomp(df)
-df_pca
-plot(df_pca$x[,1], df_pca$x[,2])
+plot(pred_pca$x[,1], pred_pca$x[,2])
 
-df_out <- as.data.frame(df_pca$x)
+# Scree Plot
+fviz_eig(pred_pca) 
+
+# PCA: coloured by dataset
+df_out <- as.data.frame(pred_pca$x) # convert to df for ggplot
 df_out$group <- sapply(strsplit(as.character(pred$dataset), "_"), "[[", 1 )
 head(df_out)
+class(df_out)
 
-
-percentage <- round(df_pca$sdev / sum(df_pca$sdev) * 100, 2)
+# calculate percentage for each axis
+percentage <- round(df_out$sdev / sum(df_out$sdev) * 100, 2)
 percentage <- paste( colnames(df_out), "(", paste( as.character(percentage), "%", ")", sep="") )
 
 p<-ggplot(df_out,aes(x=PC1,y=PC2, colour=group))
@@ -83,8 +100,26 @@ p<-p+geom_point() + theme + xlab(percentage[1]) + ylab(percentage[2])
 p
 
 # Biplot
-library("ggbiplot")
-pca.bi <- prcomp(wine,scale.=TRUE)
-p<-ggbiplot(wine.pca, obs.scale=1, var.scale=1, groups=wine.class, ellipse=TRUE, circle=TRUE)
-p<-p+scale_color_discrete(name="")
+autoplot(pred_pca, loadings = TRUE, loadings.label = TRUE,
+         data = pred, colour = 'dataset') + theme
+
+# K-means clustering?
+autoplot(kmeans(pred[-1], 4), data = pred, # 4 means? How to decide on this?
+         label = TRUE, label.size = 3, frame = TRUE) + theme
+kmeans(pred[-1], 4)
+############################################################
+
+
+
+############################################################
+### PCA for each Region ###
+
+# split intro regions
+pred %>%
+  group_by(dataset) %>%
+  nest() %>%
+  select(data) %>%
+  unlist(recursive = F) %>%
+  map(identity)
+
 
