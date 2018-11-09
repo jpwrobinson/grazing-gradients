e2ce992fc73fa07917e4ff3c1f32f3bf36e5fad2
James Robinson <jpwrobinson@gmail.com>
2018-11-09 16:51:56 +0000

    james adding to fig2 stuff

--- scripts/figures/biomass_main_fig.R
+++ scripts/figures/biomass_main_fig.R
@@ -1,13 +1,13 @@
 
 
-pdf(file = "scripts/figures/figure2_panels.pdf", 10, 7)
+pdf(file = "figures/figure2_panels.pdf", width=15, height=4)
 
 ## load models and predictions
 
-load("~/git_repos/grazing-gradients/results/models/biomass_m.browsers.Rdata")
-load("~/git_repos/grazing-gradients/results/models/biomass_m.scrapers.Rdata")
-load("~/git_repos/grazing-gradients/results/models/biomass_m.grazers.Rdata")
-load("~/git_repos/grazing-gradients/results/models/biomass_m.predictions.Rdata")
+load("results/models/biomass_m.browsers.Rdata")
+load("results/models/biomass_m.scrapers.Rdata")
+load("results/models/biomass_m.grazers.Rdata")
+load("results/models/biomass_m.predictions.Rdata")
 
 
 require(gridExtra)
@@ -15,8 +15,51 @@ library(grid)
 library(lme4)
 library(sjPlot)
 library(tidyverse)
+library(cowplot)
+library(ggplot2)
 
 ## organize data to make 1st 2 panels for macroalgae and substrate#########
+
+## James - need to setup dataframe h here! I copied these lines straight from 01_biomass.Rmd
+load("data/wio_herb_benthic_merged.Rdata")
+
+## assign seychelles 2017 with mean complexity values for now - needs fixed
+pred$complexity[pred$dataset == 'Seychelles' & pred$date == 2017] <- mean(pred$complexity)
+
+# estimate mean biomass per site per FG
+h <- pred %>% 
+  ## sum biomass per FG in each transect
+  group_by(dataset, date, reef, site, management, transect, 
+           unique.id, depth, FG,
+           hard.coral, macroalgae, rubble, substrate, complexity, fish.biom) %>%
+  summarise(biom = sum(biomass.kgha)) %>%
+  ## mean FG biomass across transects at each site
+  group_by(dataset, date, reef, site, management, unique.id, depth, FG,
+           hard.coral, macroalgae,  rubble, substrate, complexity, fish.biom) %>%
+  summarise(biom = mean(biom)) 
+
+
+h<- spread(h, FG, biom, fill=0)
+colnames(h)[14:16]<-c('browser', 'grazer', 'scraper')
+h<-as.data.frame(h)
+
+## scale vars to keep covariate means = 0. This is helpful for comparing effect sizes when covariates are on different scales.
+h$hard.coral <- scale(h$hard.coral)
+h$macroalgae <- scale(h$macroalgae)
+h$complexity <- scale(h$complexity)
+h$rubble <- scale(h$rubble)
+h$substrate <- scale(h$substrate)
+h$fish.biom <- scale(h$fish.biom)
+
+## make dummy variables
+h$fish.dummy<-ifelse(h$management=='Fished', 1, 0)
+h$pristine.dummy<-ifelse(h$management=='Unfished', 1, 0)
+
+## convert biomass to log10
+h$grazerlog10<-log10(h$grazer+1)
+h$scraperlog10<-log10(h$scraper +1)
+h$browserlog10<-log10(h$browser+1)
+
 #####################################################################
 #GRAZERS
 cont.pred.master<-data.frame(hard.coral = seq(min(h$hard.coral), max(h$hard.coral), length.out=30),
@@ -101,8 +144,18 @@ p.substrate.s<-predict(object=m.scraper, newdata = nd.s, re.form=NA)
 
 
 #Panel 1: relative effect sizes of each FG (merged pic )
-g1 <- plot_models(m.scraper, m.grazer, m.browser, legend.title = "Functional group", 
-                  axis.labels = rev(labs$lab[match(names(od), labs$model)])) 
+
+labs<-data.frame(lab=c('Hard coral', 'Macroalgae', 'Rubble', 'Available substrate', 'Complexity', 'Fishable biomass', 'Fished',   'Pristine'),
+                 model=c('hard.coral', 'macroalgae', 'rubble', 'substrate','complexity', 'fish.biom', 'fish.dummy', 'pristine.dummy'))
+
+## extract correct order of names according to increasing parameter estimate size
+od<-sort(fixef(m.grazer), decreasing=T)
+od<-od[-which(names(od) == '(Intercept)')]
+
+g1 <- plot_models(m.scraper, m.grazer, m.browser, legend.title = "", 
+                  m.labels=c("Grazer", "Scraper", "Browser"),
+                  axis.labels = rev(labs$lab[match(names(od), labs$model)])) +
+          theme(legend.position=c(0.6, 0.4)) 
 
 
 #Panel 2: macroalgae
@@ -124,10 +177,13 @@ p.algae <- rbind(p.algae.g, p.algae.b, p.algae.s)
 p.algae <- cbind(p.algae, cont.pred.master$macroalgae)
 colnames(p.algae)[3] <- "x"
 
-library(ggplot2)
-g2 <- ggplot(p.algae, aes(x, y, group=model, color = model)) + geom_point() + geom_line() + labs(title = "Macroalgae") +
- xlab("Benthic cover (%)") + ylab("Log10 Biomass (kg/ha)")
-g2
+
+g2 <- ggplot(p.algae, aes(x, y, group=model, color = model)) + 
+        geom_line() + 
+        labs(title = "") +
+        guides(col=F) +
+        xlab("Macroalgae (%)") + ylab("Log10 Biomass (kg/ha)")
+
 
 
 
@@ -151,9 +207,12 @@ p.substrate <- cbind(p.substrate, cont.pred.master$substrate)
 colnames(p.substrate)[3] <- "x"
 
 
-g3 <- ggplot(p.substrate, aes(x, y, group=model, color = model)) + geom_point() + geom_line() + labs(title = "Substrate") +
-  xlab("Available substrate") + ylab("Log10 Biomass (kg/ha)")
-g3
+g3 <- ggplot(p.substrate, aes(x, y, group=model, color = model)) + 
+        geom_line() + 
+        labs(title = "") +
+        guides(col=F) +
+        xlab("Available substrate") + ylab("")
+
 
 
 ###############################
@@ -194,16 +253,22 @@ colnames(fish.master)[9] <- "y"
 
 #### plot all the models on one graph 
 
-g4 <- ggplot(fish.master, aes(x, y, group=model, color = model)) + geom_point() + geom_line() + labs(title = "Fishable biomass") +
-  xlab("Fishable biomass (kg/ha)") + ylab("Log10 Biomass (kg/ha)")
-g4
+g4 <- ggplot(fish.master, aes(x, y, group=model, color = model)) + 
+        geom_line() + 
+        labs(title = "") +
+        guides(col=F) +
+        xlab("Fishable biomass (kg/ha)") + ylab("")
+
 
 
 
 ###############
 #bring in all figs in 1 pannel
 
-grid.arrange(g1, g2, g3, g4)
+#grid.arrange(g1, g2, g3, g4)
+
+##plot grid has easy labelling options
+plot_grid(g2, g3, g4, g1, nrow =1, labels=c('a', 'b', 'c', 'd'))
 
 dev.off()
 
