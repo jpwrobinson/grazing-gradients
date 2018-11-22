6a9eb2ff3949ffbcbce4dbaaa5018b0a5f461f1a
James Robinson <jpwrobinson@gmail.com>
2018-11-22 10:23:29 +0000

    scraper area model, knitted

--- scripts/cleaning/clean_scrape_area.R
+++ scripts/cleaning/clean_scrape_area.R
@@ -15,6 +15,7 @@ write.csv(area, file='data/bite-rates/scrape_sizes.csv')
 
 
 hist(area$bitearea)
+hist(log(area$bitearea))
 ## will need gamma dist in models
 
 ## model testing here, migrate to Rmd when correct
@@ -24,19 +25,32 @@ library(rethinking)
 
 ## drop zeroes
 area <- area[area$bitearea > 0,] ## 253 obs
-
+log(area.prior)
 
 scrape.m2<-map2stan(
         alist(
           bitearea ~ dgamma2(mu, scale),
-          log(mu) ~ a + B*TL + X1[species] + X2[genus],
-          X1[species] ~ dnorm(0, sigmar),
-          X2[genus] ~ dnorm(0, sigmar2),
-          a ~ dnorm(85.68, 10),
-          B ~ dnorm(0, 5),
-          scale ~ dexp(1),
-          c(sigmar, sigmar2) ~ dcauchy(0, 1)
+          log(mu) ~ a + B*TL, #+ X1[species] + X2[genus],
+          #X1[species] ~ dnorm(0, sigmar),
+          #X2[genus] ~ dnorm(0, sigmar2),
+          a ~ dnorm(4.45, 5),
+          B ~ dnorm(0, 2),
+          scale ~ dexp(5)
+          #c(sigmar, sigmar2) ~ dcauchy(0, 1)
         ),
         data=area, warmup = 1500, iter = 5000, chains =1, cores = 4)
 
 precis(scrape.m2)
+postcheck(scrape.m2)
+plot(precis(scrape.m2))
+plot(scrape.m2)
+
+save(area, scrape.m2, file = 'results/models/area_scrapers.Rdata')
+
+library(ggplot2)
+ggplot(area, aes(TL, bitearea)) + geom_point()
+
+pdf(file='figures/explore/scraper_area_byspecies.pdf')
+theme_set(theme_bw())
+ggplot(area, aes(TL, bitearea, col=genus)) + geom_point() + facet_wrap(~ species)
+dev.off()
