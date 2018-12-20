25b0209109e4fccdfc1f415eaa07d711f7d5f09f
James Robinson <jpwrobinson@gmail.com>
2018-12-20 16:17:22 +0000

    div plots for fig5, showing raw vals

--- scripts/figures/Figure4_diversity_decouple.R
+++ scripts/figures/Figure4_diversity_decouple.R
@@ -38,61 +38,87 @@ pairs2(dplyr::select_if(crop.pred, is.numeric),
   lower.panel = panel.cor, upper.panel = panel.smooth2, diag.panel=panel.hist)
 
 
+## MMI for decoupling
+# mm.crop<-mmi_tvalue(m.graze, dataset = crop.pred,  
+#     exp.names = c('biom', 'site.rarefied', 'site.evenness', 'abund'), 
+#     indicator = 'cropping.gram.ha', 
+#     ranef = c('dataset', 'reef'), family='Gamma')
 
-mm.crop<-mmi_tvalue(m.graze, dataset = crop.pred,  
-    exp.names = c('biom', 'site.rarefied', 'site.evenness', 'abund'), 
-    indicator = 'cropping.gram.ha', 
-    ranef = c('dataset', 'reef'), family='Gamma')
+## top model is:
+crop.top<-glmer(cropping.gram.ha ~ biom +
+                   #site.rarefied +
+                   site.evenness +
+                   abund +
+                   (1 | dataset/reef), crop.pred, family='Gamma'(link = 'log'))
+
 
 load("results/models/scraper_function.Rdata")
 scrapers<-h
-scrapers$grazef<-scrapers$scraping
-scrapers$scraping<-NULL
 scrapers$sp <- 'scrapers'
 
-m.scrape<-glmer(scraping ~ scale(biom) + (1 | dataset/reef), h, family='Gamma'(link = 'log'))
-
-
-nd.rich.crop<-data.frame(site.richness = seq(min(h$site.richness), max(h$site.richness), length.out=20),
-              sp.richness.raw = seq(min(h$site.richness.raw), max(h$site.richness.raw), length.out=20),
-                          hard.coral = 0, macroalgae = 0, rubble = 0, substrate = 0, complexity = 0, 
-                          fish.biom = 0, fish.dummy = 0, pristine.dummy = 0, site.size =0,
-                          dataset = 'GBR', reef='1')
-nd.rich.crop$pred<-predict(m.grazer, newdata=nd.rich.crop, re.form=NA, type='response')
-nd.rich.crop$se<-predict(m.grazer, newdata=nd.rich.crop, re.form=NA, type='response', se.fit=TRUE)$se.fit
-
-partials<-visreg::visreg(m.grazer, 'site.richness')
-richness.fit.crop<-data.frame(fit=partials$fit$visregFit, x = partials$fit$site.richness, 
-      lwr = partials$fit$visregLwr, upr = partials$fit$visregUpr,
-  richness = seq(min(h$site.richness.raw), max(h$site.richness.raw), length.out = 101))
-
-richness.points.crop<-data.frame(x=h$site.richness.raw, y = partials$res$visregRes, 
-            dataset=h$dataset, management = h$management, biom=h$biom)
-
-crop<-h
-
-## scraping data
-load(file = 'results/models/function_m_scrapers.Rdata'); h <- focal
 load(file = 'results/scraper_attributes.Rdata')
-h$site.richness.raw<-diversity.preds$richness[match(h$unique.id, diversity.preds$unique.id)]
+scrapers$site.richness<-diversity.preds$richness[match(scrapers$unique.id, diversity.preds$unique.id)]
+scrapers$site.evenness<-diversity.preds$J[match(scrapers$unique.id, diversity.preds$unique.id)]
+scrapers$site.evenness[is.na(scrapers$site.evenness)]<-0
+rare<-read.csv(file = 'results/rarefied_richness_scrapers.csv')
+scrapers$site.rarefied<-rare$qD[match(scrapers$unique.id, rare$site)]
 
-nd.rich.scrape<-data.frame(site.richness = seq(min(h$site.richness), max(h$site.richness), length.out=20),
-              sp.richness.raw = seq(min(h$site.richness.raw), max(h$site.richness.raw), length.out=20),
-                          hard.coral = 0, macroalgae = 0, rubble = 0, substrate = 0, complexity = 0, 
-                          fish.biom = 0, fish.dummy = 0, pristine.dummy = 0, site.size =0,
+scrap.pred<-scaler(scrapers, ID=c('date', 'dataset', 'reef', 'site', 'transect', 'unique.id', 'scraping'))
+
+m.graze<-glmer(scraping ~ biom +
+                   site.rarefied +
+                   site.evenness +
+                   abund +
+                   (1 | dataset/reef), scrap.pred, family='Gamma'(link = 'log'))
+colnames(m.graze)
+pairs2(dplyr::select_if(scrap.pred, is.numeric), 
+  lower.panel = panel.cor, upper.panel = panel.smooth2, diag.panel=panel.hist)
+
+## MMI for decopuling
+# mm.scrap<-mmi_tvalue(m.graze, dataset = scrap.pred,  
+#     exp.names = c('biom', 'site.rarefied', 'site.evenness', 'abund'), 
+#     indicator = 'scraping', 
+#     ranef = c('dataset', 'reef'), family='Gamma')
+
+
+
+## top model is:
+scrap.top<-glmer(scraping ~ biom +
+                   site.rarefied +
+                   site.evenness +
+                   abund +
+                   (1 | dataset/reef), scrap.pred, family='Gamma'(link = 'log'))
+
+
+## cropping evenness
+nd.even.crop<-data.frame(site.evenness = seq(min(crop.pred$site.evenness), max(crop.pred$site.evenness), length.out=20),
+              site.evenness.raw = seq(min(croppers$site.evenness), max(croppers$site.evenness), length.out=20),
+              biom = 0, abund = 0,
                           dataset = 'GBR', reef='1')
-nd.rich.scrape$pred<-predict(m.scraper, newdata=nd.rich.scrape, re.form=NA, type='response')
-nd.rich.scrape$se<-predict(m.grazer, newdata=nd.rich.scrape, re.form=NA, type='response', se.fit=TRUE)$se.fit
+nd.even.crop$pred<-predict(crop.top, newdata=nd.even.crop, re.form=NA, type='response')
+nd.even.crop$se<-predict(crop.top, newdata=nd.even.crop, re.form=NA, type='response', se.fit=TRUE)$se.fit
 
-partials<-visreg::visreg(m.scraper, 'site.richness')
-richness.fit.scrape<-data.frame(fit=partials$fit$visregFit, x = partials$fit$site.richness, 
-    lwr = partials$fit$visregLwr, upr = partials$fit$visregUpr,
-  richness = seq(min(h$site.richness.raw), max(h$site.richness.raw), length.out = 101))
+crop.raw.even<-croppers %>% group_by(reef, dataset) %>% summarise(x = mean(site.evenness), y = mean(cropping.gram.ha)) 
 
-richness.points.scrape<-data.frame(x=h$site.richness.raw, y = partials$res$visregRes, 
-            dataset=h$dataset, management = h$management, biom=h$biom)
+## scraping richness
+nd.rich.scrape<-data.frame(site.rarefied = seq(min(scrap.pred$site.rarefied), max(scrap.pred$site.rarefied), length.out=20),
+              site.rarefied.raw = seq(min(scrapers$site.rarefied), max(scrapers$site.rarefied), length.out=20),
+                          biom=0, abund = 0, site.evenness= 0 ,
+                          dataset = 'GBR', reef='1')
+nd.rich.scrape$pred<-predict(scrap.top, newdata=nd.rich.scrape, re.form=NA, type='response')
+nd.rich.scrape$se<-predict(scrap.top, newdata=nd.rich.scrape, re.form=NA, type='response', se.fit=TRUE)$se.fit
 
+scrap.raw.rich<-scrapers %>% group_by(reef, dataset) %>% summarise(x = mean(site.rarefied), y = mean(scraping)) 
 
+## scraping evenness
+nd.even.scrape<-data.frame(site.evenness = seq(min(scrap.pred$site.evenness), max(scrap.pred$site.evenness), length.out=20),
+              site.evenness.raw = seq(min(scrapers$site.evenness), max(scrapers$site.evenness), length.out=20),
+                          biom=0, abund = 0, site.rarefied= 0 ,
+                          dataset = 'GBR', reef='1')
+nd.even.scrape$pred<-predict(scrap.top, newdata=nd.even.scrape, re.form=NA, type='response')
+nd.even.scrape$se<-predict(scrap.top, newdata=nd.even.scrape, re.form=NA, type='response', se.fit=TRUE)$se.fit
+
+scrap.raw.even<-scrapers %>% group_by(reef, dataset) %>% summarise(x = mean(site.evenness), y = mean(scraping)) 
 
 ## add some plotting stuff
 pal <- wesanderson::wes_palette("Zissou1", 21, type = "continuous")
@@ -100,32 +126,48 @@ cols<-c(pal[5], pal[12])
 
 
 ### predicted relationships
-g1<-ggplot(nd.rich.crop, aes(sp.richness.raw, pred)) + 
-    geom_ribbon(aes(ymin = pred - 2*se, ymax = pred + 2*se), alpha=0.1) + 
-    geom_line() +
-    geom_point(data=crop, aes(site.richness.raw, cropping.gram.ha, shape = dataset), size=3.5, alpha=0.7, col=cols[1])  +
-    labs(y = 'Partial effect on area grazed', x = 'Species richness') +
+g1<-ggplot(nd.even.crop) + 
+    geom_ribbon(aes(site.evenness.raw, pred, ymin = pred - 2*se, ymax = pred + 2*se), alpha=0.1, fill=cols[1]) + 
+    geom_line(aes(site.evenness.raw, pred), col=cols[1]) +
+    labs(y = expression(paste("algal consumption g ha"^-1,"min"^-1)),
+     x = 'Species evenness') +
     # geom_hline(yintercept=0, linetype=5, col='grey') +
     theme(legend.title=element_blank(),
           legend.position = c(0.2, 0.8)) +
-    guides(size = F)
+    guides(size = F) +
+    geom_point(data=croppers, col=cols[1], aes(site.evenness, cropping.gram.ha, shape=dataset))
 
 
-g2<-ggplot(nd.rich.scrape, aes(sp.richness.raw, pred)) + 
-    geom_ribbon(aes(ymin = pred - 2*se, ymax = pred + 2*se), alpha=0.1) + 
-    geom_line() +
-    geom_point(data=h, aes(site.richness.raw, scraping, shape = dataset), size=3.5, alpha=0.7, col=cols[2])  +
-    labs(y = '', x = 'Species richness') +
+g2<-ggplot(nd.rich.scrape) + 
+    geom_ribbon(aes(site.rarefied.raw, pred, ymin = pred - 2*se, ymax = pred + 2*se), alpha=0.1,fill=cols[2]) + 
+    geom_line(aes(site.rarefied.raw, pred), col=cols[2]) +
+    # geom_point(data=h, aes(site.rarefied, scraping, shape = dataset), size=3.5, alpha=0.7, col=cols[2])  +
+    labs(y = expression(paste('area grazed m'^2,' ha'^-1, 'min'^-1)),
+     x = 'Species richness (rarefied)') +
     # geom_hline(yintercept=0, linetype=5, col='grey') +
     theme(legend.title=element_blank(),
-          legend.position = 'none')
+          legend.position = 'none') +
+    geom_point(data=scrapers, col=cols[2],aes(site.rarefied, scraping, shape=dataset))
+
+
+g3<-ggplot(nd.even.scrape) + 
+    geom_ribbon(aes(site.evenness.raw, pred, ymin = pred - 2*se, ymax = pred + 2*se), alpha=0.1,fill=cols[2]) + 
+    geom_line(aes(site.evenness.raw, pred), col=cols[2]) +
+    # geom_point(data=h, aes(site.rarefied, scraping, shape = dataset), size=3.5, alpha=0.7, col=cols[2])  +
+    labs(y = expression(paste('area grazed m'^2,' ha'^-1, 'min'^-1)),
+     x = 'Species evenness') +
+    # geom_hline(yintercept=0, linetype=5, col='grey') +
+    theme(legend.title=element_blank(),
+          legend.position = 'none') +
+    geom_point(data=scrapers, col=cols[2],aes(site.evenness, scraping, shape=dataset))
 
 
 
 
-pdf(file='figures/Figure4_diversity.pdf', height = 4, width=9)
-plot_grid(g1, g2, labels=c('a', 'b'))
+pdf(file='figures/Figure5_diversity.pdf', height = 3, width=12)
+plot_grid(g1, g2, g3, labels=c('A', 'B', 'C'), nrow=1)
 dev.off()
+
 
 
 
